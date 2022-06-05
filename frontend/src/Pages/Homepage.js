b64302232ab65ef3556fedbbc8d9ec4eb51a83d5
SajalDevnath <sajaldevnath96@gmail.com>
2022-06-05 13:24:05 +0530

    Added Login and Sign Up form

--- frontend/src/Pages/Homepage.js
+++ frontend/src/Pages/Homepage.js
@@ -1,20 +1,27 @@
 import { Box, Container, Text } from "@chakra-ui/react";
 import React from "react";
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
+import Login from "../components/Authentication/Login";
+import Signup from "../components/Authentication/Signup";
 
 const Homepage = () => {
   return (
     <Container maxW="xl" centerContent>
-      <Box
-        d="flex"
-        justifyContent="center"
-        p={3}
-        bg={"white"}
-        w="100%"
-        m="40px 0 15px 0"
-        borderRadius="lg"
-        borderWidth="1px"
-      >
-        <Text>Messenger</Text>
+      <Box bg="white" w="100%" p={4} borderRadius="lg" borderWidth="1px">
+        <Tabs variant="soft-rounded">
+          <TabList mb="1em">
+            <Tab width="50%">Login</Tab>
+            <Tab width="50%">Sign Up</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <Login />
+            </TabPanel>
+            <TabPanel>
+              <Signup />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
       </Box>
     </Container>
   );
---
+++ frontend/src/components/Authentication/Login.js
@@ -0,0 +1,67 @@
+import {
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  InputGroup,
+  InputRightElement,
+  Show,
+  VStack,
+} from "@chakra-ui/react";
+import React, { useState } from "react";
+
+const Login = () => {
+  const [show, setShow] = useState(false);
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  const handleClick = () => setShow(!show);
+  const submitHandler = () => {};
+
+  return (
+    <VStack spacing="5px">
+      <FormControl id="email" isRequired>
+        <FormLabel>Email</FormLabel>
+        <Input
+          placeholder="Enter your email"
+          onChange={(e) => setEmail(e.target.value)}
+        ></Input>
+      </FormControl>
+      <FormControl id="password" isRequired>
+        <FormLabel>Password</FormLabel>
+        <InputGroup>
+          <Input
+            type={show ? "text" : "password"}
+            placeholder="Enter your password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <InputRightElement width="4.5rem">
+            <Button h="1.75rem" size="sm" onClick={handleClick}>
+              {show ? "Hide" : "Show"}
+            </Button>
+          </InputRightElement>
+        </InputGroup>
+      </FormControl>
+      <Button
+        colorScheme="blue"
+        width="20%"
+        style={{ marginTop: 15 }}
+        onClick={submitHandler}
+      >
+        Login
+      </Button>
+      <Button
+        colorScheme="red"
+        width="25%"
+        onClick={() => {
+          setEmail("guest@example.com");
+          setPassword("123456789");
+        }}
+      >
+        Guest User
+      </Button>
+    </VStack>
+  );
+};
+
+export default Login;
